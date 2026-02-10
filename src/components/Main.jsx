642bf33fee4860078945afea48423881a0c4b128
abdu <abdualkerim01@gmail.com>
2026-02-10 22:16:32 +0300

    Main file updated

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+function Main() {
+  return (
+    <main className="main">
+      <img
+        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f"
+        alt="Website illustration"
+        className="main-image"
+      />
+
+      <p>This website is built with React.</p>
+    </main>
+  );
+}
+
+export default Main;
