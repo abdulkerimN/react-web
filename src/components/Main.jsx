2bff8e5bd41c8d2021e1d553d5ace62adab78deb
abdu <abdualkerim01@gmail.com>
2026-02-11 22:48:22 +0300

    Main updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ function Main() {
   return (
     <main className="main">
       <img
-        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f"
+        src="https://i.pinimg.com/1200x/f4/d7/59/f4d759fbe37a3445c0dc4908450b5015.jpg"
         alt="Website illustration"
         className="main-image"
       />
